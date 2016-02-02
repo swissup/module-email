d63506bf3b89fc20f650abab4bd06768aa49ba2b
Alexander Kras'ko <0m3r.mail@gmail.com>
2016-02-02 15:12:59 +0200

    mass Enable | disable actions was added

--- Controller/Adminhtml/Email/Service/MassDisable.php
+++ Controller/Adminhtml/Email/Service/MassDisable.php
@@ -8,9 +8,9 @@ use Swissup\Email\Model\ResourceModel\Service\CollectionFactory;
 
 
 /**
- * Class MassDelete
+ * Class MassDisable
  */
-class MassDelete extends \Magento\Backend\App\Action
+class MassDisable extends \Magento\Backend\App\Action
 {
     /**
      * @var Filter
@@ -42,14 +42,18 @@ class MassDelete extends \Magento\Backend\App\Action
      */
     public function execute()
     {
+        $key = 'status';
+        $value = 0;
+        // $value = (int) $this->getRequest()->getParam('status');
         $collection = $this->filter->getCollection($this->collectionFactory->create());
         $collectionSize = $collection->getSize();
 
         foreach ($collection as $row) {
-            $row->delete();
+            $row->setData($key, $value);
+            $row->save();
         }
 
-        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been changed.', $collectionSize));
 
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
--- Controller/Adminhtml/Email/Service/MassEnable.php
+++ Controller/Adminhtml/Email/Service/MassEnable.php
@@ -8,9 +8,9 @@ use Swissup\Email\Model\ResourceModel\Service\CollectionFactory;
 
 
 /**
- * Class MassDelete
+ * Class MassEnable
  */
-class MassDelete extends \Magento\Backend\App\Action
+class MassEnable extends \Magento\Backend\App\Action
 {
     /**
      * @var Filter
@@ -42,14 +42,17 @@ class MassDelete extends \Magento\Backend\App\Action
      */
     public function execute()
     {
+        $key = 'status';
+        $value = 1;
         $collection = $this->filter->getCollection($this->collectionFactory->create());
         $collectionSize = $collection->getSize();
 
         foreach ($collection as $row) {
-            $row->delete();
+            $row->setData($key, $value);
+            $row->save();
         }
 
-        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been changed.', $collectionSize));
 
         /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
         $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
