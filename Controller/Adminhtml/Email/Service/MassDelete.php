c2a9e615c7e0e10fc036631c472071a2baf7a8bb
Alexander Kras'ko <0m3r.mail@gmail.com>
2016-02-02 12:32:11 +0200

    mass Delete action was added

---
+++ Controller/Adminhtml/Email/Service/MassDelete.php
@@ -0,0 +1,59 @@
+<?php
+namespace Swissup\Email\Controller\Adminhtml\Email\Service;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Backend\App\Action\Context;
+use Magento\Ui\Component\MassAction\Filter;
+// use Swissup\Email\Model\ResourceModel\Service\Grid\CollectionFactory;
+use Swissup\Email\Model\ResourceModel\Service\CollectionFactory;
+
+
+/**
+ * Class MassDelete
+ */
+class MassDelete extends \Magento\Backend\App\Action
+{
+    /**
+     * @var Filter
+     */
+    protected $filter;
+
+    /**
+     * @var CollectionFactory
+     */
+    protected $collectionFactory;
+
+    /**
+     * @param Context $context
+     * @param Filter $filter
+     * @param CollectionFactory $collectionFactory
+     */
+    public function __construct(Context $context, Filter $filter, CollectionFactory $collectionFactory)
+    {
+        $this->filter = $filter;
+        $this->collectionFactory = $collectionFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Execute action
+     *
+     * @return \Magento\Backend\Model\View\Result\Redirect
+     * @throws \Magento\Framework\Exception\LocalizedException|\Exception
+     */
+    public function execute()
+    {
+        $collection = $this->filter->getCollection($this->collectionFactory->create());
+        $collectionSize = $collection->getSize();
+
+        foreach ($collection as $row) {
+            $row->delete();
+        }
+
+        $this->messageManager->addSuccess(__('A total of %1 record(s) have been deleted.', $collectionSize));
+
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultFactory->create(ResultFactory::TYPE_REDIRECT);
+        return $resultRedirect->setPath('*/*/');
+    }
+}
--- Model/ResourceModel/Service/Collection.php
+++ Model/ResourceModel/Service/Collection.php
@@ -1,10 +1,13 @@
 <?php
 namespace Swissup\Email\Model\ResourceModel\Service;
 
-// use Swissup\Email\Model\Service;
+use Swissup\Email\Api\Data\ServiceInterface;
+use Swissup\Email\Model\Service;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
+    protected $_idFieldName = 'id';
+
     /**
      * Define resource model
      *
