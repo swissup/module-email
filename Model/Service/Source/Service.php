b144ae50b44e843711ddd940121d7bd9d9e52322
Alexander Kras'ko <0m3r.mail@gmail.com>
2016-02-09 17:47:35 +0200

    'Default Transport Email Service' config option was added

---
+++ Model/Service/Source/Service.php
@@ -0,0 +1,41 @@
+<?php
+namespace Swissup\Email\Model\Service\Source;
+
+use Swissup\Email\Model\Service\CollectionFactory as ServiceCollectionFactory;
+
+class Service implements \Magento\Framework\Data\OptionSourceInterface
+{
+    /**
+     * @var ServiceCollectionFactory
+     */
+    protected $serviceCollectionFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ServiceCollectionFactory $serviceCollectionFactory
+     */
+    public function __construct(ServiceCollectionFactory $serviceCollectionFactory)
+    {
+        $this->serviceCollectionFactory = $serviceCollectionFactory;
+    }
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options[] = ['label' => 'Magento built-in service', 'value' => ''];
+        $serviceCollection = $this->serviceCollectionFactory->create();//->load()->getItems();
+
+        foreach ($serviceCollection as $service) {
+            $options[] = [
+                'label' => $service->getName(),
+                'value' => $service->getId(),
+            ];
+        }
+        return $options;
+    }
+}
