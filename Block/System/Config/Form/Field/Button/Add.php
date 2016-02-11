146572bc8c089926b1c2d68c6e979b086046ac8d
Alexander Kras'ko <0m3r.mail@gmail.com>
2016-02-11 11:43:10 +0200

    addService button was added in config

---
+++ Block/System/Config/Form/Field/Button/Add.php
@@ -0,0 +1,35 @@
+<?php
+namespace Swissup\Email\Block\System\Config\Form\Field\Button;
+
+/**
+ * Class
+ */
+class Add extends \Magento\Config\Block\System\Config\Form\Field
+{
+    /**
+     * Retrieve element HTML markup
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    protected function _getElementHtml(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        /** @var \Magento\Backend\Block\Widget\Button $buttonBlock  */
+        $buttonBlock = $this->getForm()->getLayout()->createBlock('Magento\Backend\Block\Widget\Button');
+
+        $params = [
+            'website' => $buttonBlock->getRequest()->getParam('website'),
+        ];
+
+        $url = $this->getUrl("adminhtml/email_service/new", $params);
+        $data = [
+            'id' => 'add_new_email_service_button',
+            'label' => __('Add Email Service'),
+            'onclick' => "setLocation('" . $url . "')",
+        ];
+
+        $html = $buttonBlock->setData($data)->toHtml();
+        return $html;
+    }
+}
